9004ed9154e305c75d4980247092a5985e962122
Patrick Nolan <patrick@MBP-von-Patrick.fritz.box>
2025-03-26 13:11:54 +0100

    🔧 Fix: Update Tailwind and PostCSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: ['bg-white', 'text-black', 'text-white'],
   darkMode: 'class',
   theme: {
     extend: {
